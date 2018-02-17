10ee5588b601b9f87bb383d79431f797ea3bccca
hippy <hippy2981@gmail.com>
2018-02-16 17:06:30 -0800

    Array: Add Array<T>.Empty() extension

---
+++ rm.Extensions/ArrayExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rm.Extensions
+{
+	/// <summary>
+	/// Array{T} extensions.
+	/// </summary>
+	public static class Array<T>
+	{
+		/// <summary>
+		/// Returns empty T array.
+		/// </summary>
+		public static T[] Empty => Array.Empty<T>();
+	}
+}
---
+++ rm.ExtensionsTest/ArrayExtensionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using rm.Extensions;
+
+namespace rm.ExtensionsTest
+{
+	[TestFixture]
+	public class ArrayExtensionTest
+	{
+		[Test]
+		public void EmptyTest01()
+		{
+			var empty = Array<int>.Empty;
+			var expected = Array.Empty<int>();
+			Assert.AreSame(expected, empty);
+			Assert.AreSame(Array<int>.Empty, Array<int>.Empty);
+		}
+	}
+}
