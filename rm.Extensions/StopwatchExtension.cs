d90e3b45eba7b7cd51c2ff0efcc3f82d0773ec89
hippy <hippy2981@gmail.com>
2018-03-06 14:14:46 -0800

    Stopwatch: Add Elaspsed extensions

---
+++ rm.Extensions/StopwatchExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace rm.Extensions
+{
+	/// <summary>
+	/// Stopwatch extensions.
+	/// </summary>
+	public static class StopwatchExtension
+	{
+		public static long ElapsedTicks(this Stopwatch sw)
+		{
+			return sw.ElapsedTicks;
+		}
+		public static long ElapsedMilliseconds(this Stopwatch sw)
+		{
+			return sw.ElapsedMilliseconds;
+		}
+		public static long ElapsedSeconds(this Stopwatch sw)
+		{
+			return sw.ElapsedMilliseconds / 1000;
+		}
+	}
+}
---
+++ rm.ExtensionsTest/StopwatchExtensionTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using rm.Extensions;
+
+namespace rm.ExtensionsTest
+{
+	[TestFixture]
+	public class StopwatchExtensionTest
+	{
+		[Test]
+		public void Elaspsed_Compile01()
+		{
+			// flaky tests as dependent on time so just put calls for compile safety.
+			var sw = Stopwatch.StartNew();
+			//Assert.AreEqual(sw.ElapsedTicks, sw.ElapsedTicks());
+			sw.ElapsedTicks();
+			//Assert.AreEqual(sw.ElapsedMilliseconds, sw.ElapsedMilliseconds());
+			sw.ElapsedMilliseconds();
+			//Assert.AreEqual(sw.ElapsedMilliseconds() / 1000, sw.ElapsedSeconds());
+			sw.ElapsedSeconds();
+		}
+	}
+}
