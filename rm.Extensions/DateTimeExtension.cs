610c80d5c06231b6c8dd40e334ac9b0ca2021b60
ryhry <ryan.hryczynski@gmail.com>
2018-10-29 11:20:09 -0700

    Add ordinal date time extension method

--- rm.Extensions/DateTimeExtension.cs
+++ rm.Extensions/DateTimeExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlTypes;
+using System.Linq;
 
 namespace rm.Extensions
 {
@@ -49,6 +50,37 @@ namespace rm.Extensions
 		public static DateTime AsUtcKind(this DateTime datetime)
 		{
 			return DateTime.SpecifyKind(datetime, DateTimeKind.Utc);
+		}
+
+		/// <summary>
+		/// Returns the date in ordinal format
+		/// </summary>
+		public static string ToYearMonthOrdinal(this DateTime date)
+		{
+			string suffix;
+
+			if (new[] { 11, 12, 13 }.Contains(date.Day))
+			{
+				suffix = "th";
+			}
+			else if (date.Day % 10 == 1)
+			{
+				suffix = "st";
+			}
+			else if (date.Day % 10 == 2)
+			{
+				suffix = "nd";
+			}
+			else if (date.Day % 10 == 3)
+			{
+				suffix = "rd";
+			}
+			else
+			{
+				suffix = "th";
+			}
+
+			return $"{date.ToString("MMMM d")}{suffix}";
 		}
 	}
 }
