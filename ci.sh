2631f34b487b5d0636cc100d01722dec5a8d8fa4
hippy <hippy2981@gmail.com>
2020-11-27 12:36:44 -0800

    test: Target new framework
    net5.0

--- ci.sh
+++ ci.sh
@@ -5,9 +5,9 @@ dotnet clean -c Release
 # netcoreapp3.1 only as test csproj also targets others
 dotnet build rm.Extensions.sln \
 	-c Release \
-	-f netcoreapp3.1
+	-f net5.0
 dotnet test tests/rm.ExtensionsTest/rm.ExtensionsTest.csproj \
 	-c Release --no-build --no-restore \
-	-f netcoreapp3.1 \
+	-f net5.0 \
 	-v normal \
 	--filter "TestCategory!=very.slow"
