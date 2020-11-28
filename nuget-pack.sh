a39410d85e724c9a8d016a59bc77868aca533ee0
hippy <hippy2981@gmail.com>
2020-11-27 16:59:14 -0800

    build: Add build metadata to version
    See
    https://docs.microsoft.com/en-us/nuget/concepts/package-versioning#semantic-versioning-200

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -12,11 +12,13 @@ if [[ -z "$version" ]]; then
 fi
 
 tag="nuget-$version"
+commit_hash=$(git rev-parse @)
+metadata="$commit_hash"
 
 dotnet pack src/rm.Extensions/rm.Extensions.csproj \
 	-c Release \
 	--include-symbols //p:SymbolPackageFormat=snupkg \
 	-o .nupkg/ \
-	//p:PackageVersion="$version" \
+	//p:PackageVersion="$version+$metadata" \
 	//p:PackageReleaseNotes="tag: $tag" \
 	&& git tag "$tag" -m "Create nuget tag $tag"
