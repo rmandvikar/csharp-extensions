97535b126e59ae03fd6c36f4fdd904e8df484054
hippy <hippy2981@gmail.com>
2021-09-05 01:01:49 -0700

    guid: Add ToByteArrayMatchingStringRepresentation()

---
+++ src/rm.Extensions/GuidExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace rm.Extensions
+{
+	/// <summary>
+	/// Guid extensions.
+	/// </summary>
+	public static class GuidExtensions
+	{
+		/// <summary>
+		/// Returns a 16-element byte array that contains the value of this instance
+		/// matching its string representation (endian-agnostic).
+		/// <para></para>
+		/// see https://stackoverflow.com/questions/9195551/why-does-guid-tobytearray-order-the-bytes-the-way-it-does
+		/// <remarks>
+		/// Note: The byte[] returned by <see cref="ToByteArrayMatchingStringRepresentation(Guid)"/> will not yield
+		/// the original Guid with <see cref="Guid(byte[])"/> ctor.
+		/// </remarks>
+		/// </summary>
+		public static byte[] ToByteArrayMatchingStringRepresentation(this Guid guid)
+		{
+			var bytes = guid.ToByteArray();
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(bytes, 0, 4);
+				Array.Reverse(bytes, 4, 2);
+				Array.Reverse(bytes, 6, 2);
+			}
+			return bytes;
+		}
+	}
+}
---
+++ tests/rm.ExtensionsTest/GuidExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using rm.Extensions;
+
+namespace rm.ExtensionsTest
+{
+	[TestFixture]
+	public class GuidExtensionsTest
+	{
+		[Test]
+		public void ToByteArrayMatchingStringRepresentation_01()
+		{
+			Console.WriteLine($"BitConverter.IsLittleEndian: {BitConverter.IsLittleEndian}");
+			var guidString = "8843eb18-45d9-4528-a4e8-62e277a26629";
+			var guid = Guid.Parse(guidString);
+
+			var bytes = guid.ToByteArrayMatchingStringRepresentation();
+
+			var guidStringRoundtrip =
+				Guid.Parse(
+					BitConverter.ToString(bytes)
+						.Replace("-", "")
+						.ToLowerInvariant())
+					.ToString("D");
+			Assert.AreEqual(guidString, guidStringRoundtrip);
+		}
+	}
+}
