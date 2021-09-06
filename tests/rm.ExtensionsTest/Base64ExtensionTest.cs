7c785e545e33e44d16732cebecc91cd952b2ccdc
hippy <hippy2981@gmail.com>
2021-09-06 02:22:48 -0700

    base64Url: Add s.ToUtf8Bytes().Base64UrlEncode(), base64.Base64UrlDecode().ToUtf8String()

--- tests/rm.ExtensionsTest/Base64ExtensionTest.cs
+++ tests/rm.ExtensionsTest/Base64ExtensionTest.cs
@@ -25,5 +25,25 @@ namespace rm.ExtensionsTest
 		{
 			Assert.AreEqual(s, base64.Base64Decode().ToUtf8String());
 		}
+
+		[Test]
+		[TestCase("Man", "TWFu")]
+		[TestCase("Woman", "V29tYW4")]
+		[TestCase("light work.", "bGlnaHQgd29yay4")]
+		[TestCase("light work", "bGlnaHQgd29yaw")]
+		public void Base64UrlEncode_01(string s, string base64Url)
+		{
+			Assert.AreEqual(base64Url, s.ToUtf8Bytes().Base64UrlEncode());
+		}
+
+		[Test]
+		[TestCase("TWFu", "Man")]
+		[TestCase("V29tYW4", "Woman")]
+		[TestCase("bGlnaHQgd29yay4", "light work.")]
+		[TestCase("bGlnaHQgd29yaw", "light work")]
+		public void Base64UrlDecode_01(string base64Url, string s)
+		{
+			Assert.AreEqual(s, base64Url.Base64UrlDecode().ToUtf8String());
+		}
 	}
 }
