d11e9bdf49b3a3ab0ef353f6c265ea5f71da255a
hippy <hippy2981@gmail.com>
2021-09-06 01:35:58 -0700

    base64: Add s.ToUtf8Bytes().Base64Encode(), base64.Base64Decode().ToUtf8String()

---
+++ src/rm.Extensions/Base64Extension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace rm.Extensions
+{
+	/// <summary>
+	/// Base64 extensions.
+	/// </summary>
+	public static class Base64Extension
+	{
+		public static string Base64Encode(this byte[] bytes)
+		{
+			return Convert.ToBase64String(bytes);
+		}
+
+		public static byte[] Base64Decode(this string base64)
+		{
+			return Convert.FromBase64String(base64);
+		}
+	}
+}
---
+++ tests/rm.ExtensionsTest/Base64ExtensionTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using rm.Extensions;
+
+namespace rm.ExtensionsTest
+{
+	[TestFixture]
+	public class Base64ExtensionTest
+	{
+		[Test]
+		[TestCase("Man", "TWFu")]
+		[TestCase("Woman", "V29tYW4=")]
+		[TestCase("light work.", "bGlnaHQgd29yay4=")]
+		[TestCase("light work", "bGlnaHQgd29yaw==")]
+		public void Base64Encode_01(string s, string base64)
+		{
+			Assert.AreEqual(base64, s.ToUtf8Bytes().Base64Encode());
+		}
+
+		[Test]
+		[TestCase("TWFu", "Man")]
+		[TestCase("V29tYW4=", "Woman")]
+		[TestCase("bGlnaHQgd29yay4=", "light work.")]
+		[TestCase("bGlnaHQgd29yaw==", "light work")]
+		public void Base64Decode_01(string base64, string s)
+		{
+			Assert.AreEqual(s, base64.Base64Decode().ToUtf8String());
+		}
+	}
+}
