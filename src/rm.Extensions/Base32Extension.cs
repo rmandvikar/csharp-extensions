0a2c717f11188bba88f2352f5f9f89ee188af552
hippy <hippy2981@gmail.com>
2021-09-07 23:27:50 -0700

    base32: Add Base32 impl (Douglas Crockford)

---
+++ src/rm.Extensions/Base32Extension.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace rm.Extensions
+{
+	/// <summary>
+	/// Base32 extensions.
+	/// </summary>
+	public static class Base32Extension
+	{
+		private static readonly Base32DouglasCrockford base32ConverterDouglasCrockford = new Base32DouglasCrockford();
+
+		/// <inheritdoc cref="Base32DouglasCrockford.Encode(byte[])"/>.
+		[DebuggerStepThrough]
+		public static string Base32Encode(this byte[] bytes)
+		{
+			return base32ConverterDouglasCrockford.Encode(bytes);
+		}
+
+		/// <inheritdoc cref="Base32DouglasCrockford.Decode(string)"/>.
+		[DebuggerStepThrough]
+		public static byte[] Base32Decode(this string base32)
+		{
+			return base32ConverterDouglasCrockford.Decode(base32);
+		}
+	}
+}
