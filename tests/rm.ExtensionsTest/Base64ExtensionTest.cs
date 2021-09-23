a2e4d250d2c5127a8ae99a8dd99fbcb93844c309
hippy <hippy2981@gmail.com>
2021-09-22 20:59:31 -0700

    base64: Add perf tests

--- tests/rm.ExtensionsTest/Base64ExtensionTest.cs
+++ tests/rm.ExtensionsTest/Base64ExtensionTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
 using rm.Extensions;
 
 namespace rm.ExtensionsTest
@@ -6,6 +8,8 @@ namespace rm.ExtensionsTest
 	[TestFixture]
 	public class Base64ExtensionTest
 	{
+		private const int iterations = 1_000_000;
+
 		[Test]
 		[TestCase("Man", "TWFu")]
 		[TestCase("Woman", "V29tYW4=")]
@@ -44,6 +48,36 @@ namespace rm.ExtensionsTest
 		public void Base64UrlDecode_01(string base64Url, string s)
 		{
 			Assert.AreEqual(s, base64Url.Base64UrlDecode().ToUtf8String());
+		}
+
+		[Explicit]
+		[Test]
+		[Category("slow")]
+		public void Perf_Base64Encode()
+		{
+			var bytes = "The quick brown fox jumps over the lazy dog.".ToUtf8Bytes();
+			var sw = Stopwatch.StartNew();
+			for (int i = 0; i < iterations; i++)
+			{
+				var base64 = bytes.Base64Encode();
+			}
+			sw.Stop();
+			Console.WriteLine(sw.ElapsedMilliseconds);
+		}
+
+		[Explicit]
+		[Test]
+		[Category("slow")]
+		public void Perf_Base64Decode()
+		{
+			var base64 = "ahm6a83henmp6ts0c9s6yxve41k6yy10d9tptw3k41qqcsbj41t6gs90dhgqmy90chqpebg=";
+			var sw = Stopwatch.StartNew();
+			for (int i = 0; i < iterations; i++)
+			{
+				var bytes = base64.Base64Decode();
+			}
+			sw.Stop();
+			Console.WriteLine(sw.ElapsedMilliseconds);
 		}
 	}
 }
