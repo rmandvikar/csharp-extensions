24b298ee402d67c0c0dd6a331c6dba0e25a7e0f8
hippy <hippy2981@gmail.com>
2021-11-01 14:16:09 -0700

    decimal: Fix decimal.TruncateTo(digits)

--- src/rm.Extensions/DecimalExtension.cs
+++ src/rm.Extensions/DecimalExtension.cs
@@ -12,9 +12,14 @@ namespace rm.Extensions
 		/// </summary>
 		public static decimal TruncateTo(this decimal n, uint digits)
 		{
-			var factor = (int)Math.Pow(10, digits);
-			var d = Math.Truncate(n * factor) / factor;
-			return d;
+			// note: Math.Round(n, digits, ...) doesn't work
+			var wholePart = decimal.Truncate(n);
+
+			var decimalPart = n - wholePart;
+			var factor = checked(Math.Pow(10, digits));
+			var decimalPartTruncated = Math.Truncate(decimalPart * (decimal)factor) / (decimal)factor;
+
+			return wholePart + decimalPartTruncated;
 		}
 	}
 }
